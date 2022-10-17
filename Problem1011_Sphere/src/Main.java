24378bff8a1241d57d83becaf681aa42924c63a6
Tyrion1606 <94870055+Tyrion1606@users.noreply.github.com>
2022-10-17 15:35:34 -0300

    .

--- Problem1011_Sphere/src/Main.java
+++ Problem1011_Sphere/src/Main.java
@@ -1,5 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    static final double pi = 3.14159;
+
+    public static void main(String[] args) throws IOException {
+        Scanner UserImput = new Scanner(System.in);
+        double radius = UserImput.nextInt();
+
+        double SphereVolume = (4.0/3.0)*pi*(Math.pow(radius, 3));
+        System.out.printf("VOLUME = %.3f\n", SphereVolume);
+
     }
+
 }
